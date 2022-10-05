785ad88ab1efa38f3e1266c2c9186f3a282244d6
ZuesBot <115041980+ZuesBot@users.noreply.github.com>
2022-10-05 16:03:04 -0500

    Update RootDialog.cs
    Cleaning up bot

--- Dialogs/RootDialog.cs
+++ Dialogs/RootDialog.cs
@@ -1,13 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.AI.QnA.Dialogs;
 using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Dialogs.Choices;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.BotBuilderSamples.Dialogs
@@ -31,63 +28,16 @@ namespace Microsoft.BotBuilderSamples.Dialogs
         {
             AddDialog(new QnAMakerBaseDialog(services, configuration));
 
-            var waterfallSteps = new WaterfallStep[]
-            {
-                InitialStepAsync,
-                CertLevelStepAsync,
-                CertCourseStepAsync,
-                SummaryStepAsync,
-            };
-
-            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
-            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
-            //AddDialog(new WaterfallDialog(InitialDialog)
-            //   .AddStep(InitialStepAsync));
+            AddDialog(new WaterfallDialog(InitialDialog)
+               .AddStep(InitialStepAsync));
 
             // The initial child Dialog to run.
-            InitialDialogId = nameof(WaterfallDialog);
+            InitialDialogId = InitialDialog;
         }
 
         private async Task<DialogTurnResult> InitialStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             return await stepContext.BeginDialogAsync(nameof(QnAMakerDialog), null, cancellationToken);
         }
-
-        private static async Task<DialogTurnResult> CertLevelStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        {
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-            // Running a prompt here means the next WaterfallStep will be run when the user's response is received.
-            return await stepContext.PromptAsync(nameof(ChoicePrompt),
-                new PromptOptions
-                {
-                    Prompt = MessageFactory.Text("Please enter your certification level"),
-                    Choices = ChoiceFactory.ToChoices(new List<string> { "Beginner", "Intermediate", "Expert" }),
-                }, cancellationToken);
-        }
-
-        private static async Task<DialogTurnResult> CertCourseStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        {
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
-            // Running a prompt here means the next WaterfallStep will be run when the user's response is received.
-            return await stepContext.PromptAsync(nameof(ChoicePrompt),
-                new PromptOptions
-                {
-                    Prompt = MessageFactory.Text("Please select you course:"),
-                    Choices = ChoiceFactory.ToChoices(new List<string> { "AZ-900", "AZ-901", "AZ-902" }),
-                }, cancellationToken);
-
-            //stepContext.Values["course"] = (string)stepContext.Result;
-        }
-
-        private async Task<DialogTurnResult> SummaryStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
-        {
-            await stepContext.Context.SendActivityAsync(MessageFactory.Text("https://learn.microsoft.com/en-us/certifications/exams/az-900"), cancellationToken);
-
-
-            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is the end.
-            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
-        }
-
-
     }
 }
